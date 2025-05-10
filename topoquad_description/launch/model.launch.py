4a4247b69fae348e4ba3c42b8124f65f5c0548ed
michikawa07_ <michikawa.ryohei@gmail.com>
2025-05-11 02:37:58 +0900

    add urdf for rviz

--- topoquad_description/launch/model.launch.py
+++ topoquad_description/launch/model.launch.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-#
-# Copyright 2022 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Darby Lim
-
 import os
 
 from launch import LaunchDescription
@@ -44,63 +26,48 @@ def generate_launch_description():
         print('Can not launch fake robot in Raspberry Pi')
         return LaunchDescription([])
 
-    prefix = LaunchConfiguration('prefix')
-    use_gui = LaunchConfiguration('use_gui')
+    packages_name = "topoquad_description"
+    xacro_file_name = "topoquad.xacro"
+    rviz_file_name = "model.rviz"
 
     urdf_file = Command(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
             PathJoinSubstitution(
-                [
-                    FindPackageShare('topoquad_description'),
-                    'urdf',
-                    'topoquad.urdf.xacro'
-                ]
+                [FindPackageShare(packages_name), "urdf", xacro_file_name]
             ),
-            ' ',
-            'prefix:=',
-            prefix,
-            ' ',
-            'use_fake_hardware:=',
-            'False',
         ]
     )
+    robot_description = {"robot_description": urdf_file}
 
     rviz_config_file = PathJoinSubstitution(
-        [
-            FindPackageShare('topoquad_description'),
-            'rviz',
-            'model.rviz'
-        ]
+        [FindPackageShare(packages_name), "rviz", rviz_file_name]
     )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'prefix',
-            default_value='""',
-            description='Prefix of the joint and link names'),
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+    joint_state_pub_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+    )
 
-        DeclareLaunchArgument(
-            'use_gui',
-            default_value='true',
-            description='Run joint state publisher gui node'),
+    nodes = [
+        rviz_node,
+        robot_state_pub_node,
+        joint_state_pub_gui_node,
+    ]
 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{'robot_description': urdf_file}],
-            output='screen'),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            arguments=['-d', rviz_config_file],
-            output='screen'),
-
-        Node(
-            package="joint_state_publisher_gui",
-            executable="joint_state_publisher_gui",
-            condition=IfCondition(use_gui)),
-    ])
-u
+    return LaunchDescription(nodes)
