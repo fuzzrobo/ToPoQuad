6d099f05c74e13885a7fe473109040b02727a677
Michikawa07 <michikawa.ryohei@gmail.com>
2025-05-10 22:23:21 +0900

    rename leg_node

--- topoquad_master/launch/spider_test.launch.py
+++ topoquad_master/launch/spider_test.launch.py
@@ -28,9 +28,9 @@ def generate_launch_description():
     
     leg_node = Node(
         package='topoquad_master',
-        executable='leg_node_smooth',
+        executable='leg_node_vel_base',
         namespace='ns',
-        name='leg_node_smooth',
+        name='leg_node_vel_base',
         output='screen',
         parameters=[topoquad_master_config]
     )
