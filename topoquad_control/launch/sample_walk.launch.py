94f2b1b5107ca77cdae69cae81efbb9a635c37b0
michikawa07 <michikawa.ryohei@gmail.com>
2026-03-24 14:36:39 +0900

    adjust param

--- topoquad_control/launch/sample_walk.launch.py
+++ topoquad_control/launch/sample_walk.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
                 name="walk_sample",
                 namespace="topoquad",
                 output="screen",
-                parameters=[{"r": 0.00, "s": 0.04, "h": 0.02, "x_offset": 0.08, "y_offset": 0.09, "z_offset": 0.08}],  # r: 周回軌道の半径, s: 左右方向の歩幅オフセット, h: 上下方向の脚上げ高さ, x/y/z_offset: 足先軌道中心の各軸オフセット量
+                parameters=[{"r": 0.00, "s": 0.04, "h": 0.015, "x_offset": 0.095, "y_offset": 0.09, "z_offset": 0.085}],  # r: 周回軌道の半径, s: 左右方向の歩幅オフセット, h: 上下方向の脚上げ高さ, x/y/z_offset: 足先軌道中心の各軸オフセット量
                 condition=IfCondition(is_topoquad),
             ),
             Node(
